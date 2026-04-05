6edecb55f89f4814d68c9f81fa6e63a7fbc1ee35
Alexandros Karayiannis <alexandros.karayiannis@student.nhlstenden.com>
2026-04-05 16:16:42 +0200

    Added NextSlideCommand class to go to the next slide

---
+++ src/com/nhlstenden/jabberpoint/NextSlideCommand.java
@@ -0,0 +1,13 @@
+// <p>Command to advance to the next slide.</p>
+
+public class NextSlideCommand implements Command {
+	private Presentation presentation;
+
+	public NextSlideCommand(Presentation presentation) {
+		this.presentation = presentation;
+	}
+
+	public void execute() {
+		presentation.nextSlide();
+	}
+}
